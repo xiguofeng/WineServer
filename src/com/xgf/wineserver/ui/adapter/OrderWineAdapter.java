a4989c2886c1e72d91b0f429fb1c873e47a4f7d5
xgf <xiguofeng@163.com>
2015-11-17 20:10:42 +0800

    update ：add type color

--- src/com/xgf/wineserver/ui/adapter/OrderWineAdapter.java
+++ src/com/xgf/wineserver/ui/adapter/OrderWineAdapter.java
@@ -24,6 +24,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -102,10 +103,23 @@ public class OrderWineAdapter extends BaseAdapter {
 					.findViewById(R.id.list_order_received_auth_btn);
 			holder.mWineLl = (LinearLayout) convertView
 					.findViewById(R.id.list_order_group_wine_ll);
+			holder.mOrderItemBgRl = (RelativeLayout) convertView
+					.findViewById(R.id.list_order_group_bg_rl);
 
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
+		}
+		
+		holder.mOrderItemBgRl.setBackgroundColor(mContext.getResources()
+				.getColor(R.color.white));
+		if (!TextUtils.isEmpty(((ArrayList<Order>) mMap.get(MsgResult.ORDER_TAG)).get(
+				position).getOrderType())) {
+			if ("2".equals(((ArrayList<Order>) mMap.get(MsgResult.ORDER_TAG)).get(
+					position).getOrderType())) {
+				holder.mOrderItemBgRl.setBackgroundColor(mContext
+						.getResources().getColor(R.color.orange_bg));
+			}
 		}
 
 		final int tempPosition = position;
@@ -254,6 +268,8 @@ public class OrderWineAdapter extends BaseAdapter {
 		public EditText mCodeEt;
 
 		public LinearLayout mWineLl;
+		
+		public RelativeLayout mOrderItemBgRl;
 
 	}
 
